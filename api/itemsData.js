925f504ffb1e681d92b3fa3edd21ad062e8d0cfc
Shalane Proctor <shalane088@gmail.com>
2022-08-27 11:50:11 -0500

    Post apis added & added PostCard.js file

--- api/itemsData.js
+++ api/itemsData.js
@@ -1,7 +1,51 @@
-import React from 'react';
+import axios from 'axios';
+import { clientCredentials } from '../utils/client';
 
-export default function itemsData() {
-  return (
-    <div>itemsData</div>
-  );
-}
+const dbUrl = clientCredentials.databaseURL;
+
+const getPosts = () => new Promise((resolve, reject) => {
+  axios.get(`${dbUrl}/items.json`)
+    .then((response) => {
+      if (response?.data) {
+        resolve(Object.values(response.data));
+      } else {
+        resolve([]);
+      }
+    })
+    .catch(reject);
+});
+
+const getSinglePost = (firebaseKey) => new Promise((resolve, reject) => {
+  axios.get(`${dbUrl}/items/${firebaseKey}.json`)
+    .then((response) => resolve(response.data))
+    .catch(reject);
+});
+
+const createPosts = (postObj) => new Promise((resolve, reject) => {
+  axios.post(`${dbUrl}/items.json`, postObj)
+    .then((response) => {
+      const payload = { firebaseKey: response.data.name };
+      axios.patch(`${dbUrl}/items/${response.data.name}.json`, payload)
+        .then(resolve);
+    }).catch(reject);
+});
+
+const updatePosts = (postObj) => new Promise((resolve, reject) => {
+  axios.patch(`${dbUrl}/items/${postObj.firebaseKey}.json`, postObj)
+    .then((response) => resolve(response.data))
+    .catch(reject);
+});
+
+const deletePost = (firebaseKey) => new Promise((resolve, reject) => {
+  axios.delete(`${dbUrl}/items/${firebaseKey}.json`)
+    .then((response) => resolve(response.data))
+    .catch(reject);
+});
+
+export {
+  getPosts,
+  getSinglePost,
+  createPosts,
+  updatePosts,
+  deletePost,
+};
