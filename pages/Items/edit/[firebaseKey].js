01d76d33d20c0a43f6d740e27a602fa61423057d
Shalane Proctor <shalane088@gmail.com>
2022-09-13 13:43:03 -0500

    added edit item page, test

--- pages/Items/edit/[firebaseKey].js
+++ pages/Items/edit/[firebaseKey].js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import PostForm from '../../../components/Forms/PostForm';
+import { getSinglePost } from '../../../api/itemsData';
 
-export default function firebaseKey() {
-  return (
-    <div>firebaseKey</div>
-  );
+export default function EditPost() {
+  const [editItem, setEditItem] = useState({});
+  const router = useRouter();
+  const { firebaseKey } = router.query;
+
+  useEffect(() => {
+    getSinglePost(firebaseKey).then(setEditItem);
+  }, [firebaseKey]);
+  return <PostForm obj={editItem} />;
 }
